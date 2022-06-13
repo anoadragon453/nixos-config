bc7adb2cf87199a81bd8cdd3d80c4eb9bf9a4a75
Andrew Morgan <andrew@amorgan.xyz>
2022-06-13 09:05:17 +0100

    Ensure install.sh adds the necessary channels

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+# A script to automatically apply my config on NixOS.
+
+# We need to know the current NixOS release version
+VERSION_INFO=`nixos-version`
+# Extract just the release number (there would ideally just be a flag for this...)
+NIXOS_RELEASE=${VERSION_INFO:0:5}
+
+# Add necessary channels.
+echo "Adding channels..."
+
+# Add home-manager channel to install home-manager from.
+# This is done as root as we'll be managing home-manager from the system configuration.
+sudo nix-channel --add "https://github.com/nix-community/home-manager/archive/release-$NIXOS_RELEASE.tar.gz home-manager"
+
+echo "Symlinking configuration files..."
+
 # Backup existing /etc/configuration.nix
 sudo mv /etc/nixos/{configuration,configuration-old}.nix
 
@@ -7,7 +23,7 @@ for filename in *.nix; do
   sudo ln -s "$PWD/$filename" "/etc/nixos/$filename"
 done
 
-echo "Configuration symlinked. Running 'nixos-rebuild switch'"
+echo "Running 'nixos-rebuild switch'..."
 
 sudo nixos-rebuild switch
 
